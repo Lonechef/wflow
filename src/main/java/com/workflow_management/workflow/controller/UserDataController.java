5326f4892b22704f9c55b2a78b0b2b2970dd64af
lonechef <saumyaparikh42@gmail.com>
2024-07-29 03:47:32 +0530

    2nd Page

---
+++ src/main/java/com/workflow_management/workflow/controller/UserDataController.java
@@ -0,0 +1,20 @@
+// controller/UserDataController.java
+package com.workflow_management.workflow.controller;
+
+import com.workflow_management.workflow.model.UserData;
+import com.workflow_management.workflow.service.UserDataService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/userdata")
+public class UserDataController {
+
+    @Autowired
+    private UserDataService userDataService;
+
+    @PostMapping("/create")
+    public UserData createUserData(@RequestBody UserData userData) {
+        return userDataService.saveUserData(userData);
+    }
+}
---
+++ src/main/java/com/workflow_management/workflow/repository/UserDataRepository.java
@@ -0,0 +1,10 @@
+// repository/UserDataRepository.java
+package com.workflow_management.workflow.repository;
+
+import com.workflow_management.workflow.model.UserData;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserDataRepository extends JpaRepository<UserData, Long> {
+}
